639b3e870dd7e834a17bd11753af8ef9a34d2abb
David Gumberg <davidzgumberg@gmail.com>
2025-10-27 11:41:15 -0700

    temporarily disable some stuff

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ warnet deploy networks/2_node_bitcoin
 warnet bitcoin rpc miner createwallet "miner"
 warnet bitcoin rpc miner -rpcwallet="miner" -generate 101
 sleep 5
-warnet bitcoin rpc tank2 createwallet "miner"
-warnet bitcoin rpc tank2 -rpcwallet="miner" -generate 101
+warnet bitcoin rpc honest createwallet "miner"
+warnet bitcoin rpc honest -rpcwallet="miner" -generate 101
 # warnet run scenarios/miner_std.py --tank=miner --mature --debug
-warnet run scenarios/tx_flood.py
+# warnet run scenarios/tx_flood.py
 
